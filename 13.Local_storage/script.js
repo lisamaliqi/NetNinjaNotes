c473e13ccb6a995e3df9d8a79202e16e8163294a
lisamaliqi <lisamaliqi@hotmail.com>
2025-01-30 18:37:04 +0100

    notes on what local storage is

---
+++ 13.Local_storage/script.js
@@ -0,0 +1,11 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//                      WHAT IS LOCAL STORAGE
+/**
+ * Hur man sparar data i applikationen när användaren lämnar och kommer tillbaka 
+ * Man sparar alltså datan i användarens browser så att den finns när användaren kommer tillbaks
+ * 
+ * LocalStorage förvaras i window objektet i browsern
+ * Innehåller en lengt som säger hur mycket saker man har i local storage
+ */
